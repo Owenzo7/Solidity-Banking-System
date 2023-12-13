4e492348574f96995800ed257c06e1f274ea5ae8
Owenzo7 <owenlee545@gmail.com>
2023-12-13 17:49:55 +0300

    Just added some security considerations

--- src/Bank.sol
+++ src/Bank.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.17;
 // Took me 290102 gas to deploy this contract
 // Took me 376543 gas to deploy this contract
 //  Took me 283029 gas to deploy this contract
+// Took me 373753 gas to deploy this contract
 contract Bank {
     address private immutable i_Accountowner;
     mapping(address client => uint256 AccountBalances) S_ClientToAccountBalances;
@@ -20,15 +21,18 @@ contract Bank {
     error Bank_NotEnoughFundsToWithdraw();
     error Bank_NotEnoughFundsToTransfer();
     error Bank_TransferCallFail();
+    error Bank_ZeroaddressFail();
+    error Bank_AmountisBeloworEqualtoZero();
+    error Bank_AmountforWithdrawalBelowOrEqualtozero();
 
-    function deposit() public payable {
+    function deposit(uint256 amount) public payable {
         uint256 clientBalances = S_ClientToAccountBalances[msg.sender];
 
-        if (msg.value < MINIMUM_USD) {
+        if (amount < MINIMUM_USD) {
             revert Bank_NotEnoughFunds();
         }
 
-        clientBalances += msg.value;
+        clientBalances += amount;
         s_bankclients.push(msg.sender);
     }
 
@@ -36,6 +40,9 @@ contract Bank {
         uint256 clientBalances = S_ClientToAccountBalances[msg.sender];
         if (clientBalances < amount) {
             revert Bank_NotEnoughFundsToWithdraw();
+        }
+        if (amount <= 0) {
+            revert Bank_AmountforWithdrawalBelowOrEqualtozero();
         }
 
         clientBalances -= amount;
@@ -55,6 +62,14 @@ contract Bank {
 
         if (startingBalanceforClient < amount) {
             revert Bank_NotEnoughFundsToTransfer();
+        }
+
+        if (ToreceiverAddress == address(0)) {
+            revert Bank_ZeroaddressFail();
+        }
+
+        if (amount <= 0) {
+            revert Bank_AmountisBeloworEqualtoZero();
         }
 
         (bool callSuccess, ) = payable(msg.sender).call{value: amount}("");
@@ -84,10 +99,10 @@ contract Bank {
     }
 
     receive() external payable {
-        deposit();
+        deposit(msg.value);
     }
 
     fallback() external payable {
-        deposit();
+        deposit(msg.value);
     }
 }
--- test/unit/BankTest.t.sol
+++ test/unit/BankTest.t.sol
@@ -2,14 +2,16 @@
 
 pragma solidity ^0.8.17;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 
 import {Bank} from "../../src/Bank.sol";
 
 contract BankTest is Test {
     Bank bank;
     address USER = makeAddr("user");
+    address BOB = makeAddr("bob");
     uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant MINIMUM_VALUE = 0.0000002 ether;
 
     function setUp() public returns (Bank) {
         bank = new Bank();
@@ -20,5 +22,29 @@ contract BankTest is Test {
 
     function testAddressUserBalance() public view {
         assert(address(USER).balance == STARTING_BALANCE);
+    }
+
+    function testRevertdepositandCheckAccountBalance() public {
+        hoax(BOB, MINIMUM_VALUE);
+        vm.expectRevert();
+        bank.deposit(MINIMUM_VALUE);
+    }
+
+    function testRevertWithdraw() public {
+        vm.prank(USER);
+        console.log("================================");
+        uint256 userStartingBalance = address(USER).balance;
+        console.log("%s", userStartingBalance);
+        bank.deposit(STARTING_BALANCE);
+        console.log("================================");
+        uint256 userEndingBalanceAfterDeposit = address(USER).balance;
+        console.log("%s", userEndingBalanceAfterDeposit);
+
+        console.log("================================");
+        vm.expectRevert();
+        bank.withdraw(11 ether);
+
+        uint256 userEndingBalanceAfterWithdrawal = address(USER).balance;
+        console.log("%s", userEndingBalanceAfterWithdrawal);
     }
 }
