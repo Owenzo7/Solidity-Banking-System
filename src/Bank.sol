27527e499db10c8b4fb59f9c6e0c2b222f4027e5
Owenzo7 <owenlee545@gmail.com>
2023-12-13 13:44:56 +0300

    done with the functionalities of the bank system

--- src/Bank.sol
+++ src/Bank.sol
@@ -2,4 +2,57 @@
 
 pragma solidity ^0.8.17;
 
-contract Bank {}
+contract Bank {
+    address private immutable i_owner;
+    mapping(address client => uint256 AccountBalances) S_ClientToAccountBalances;
+    uint256 constant MINIMUM_USD = 0.012 ether;
+    address[] private s_bankclients;
+
+    constructor() {
+        msg.sender == i_owner;
+    }
+
+    error Bank_NotEnoughFunds();
+    error Bank_NotEnoughFundsToWithdraw();
+
+    function deposit() public payable {
+        if (msg.value < MINIMUM_USD) {
+            revert Bank_NotEnoughFunds();
+        }
+
+        S_ClientToAccountBalances[msg.sender] += msg.value;
+        s_bankclients.push(msg.sender);
+    }
+
+    function withdraw(uint256 amount) public payable {
+        if (S_ClientToAccountBalances[msg.sender] < amount) {
+            revert Bank_NotEnoughFundsToWithdraw();
+        }
+
+        S_ClientToAccountBalances[msg.sender] -= amount;
+    }
+
+    // View and getter functions
+
+    function getClientToAccountBalances(
+        address BankAdress
+    ) external view returns (uint256) {
+        return S_ClientToAccountBalances[BankAdress];
+    }
+
+    function getBankClient(uint256 index) external view returns (address) {
+        return s_bankclients[index];
+    }
+
+    function getOwner() external view returns (address) {
+        return i_owner;
+    }
+
+    receive() external payable {
+        deposit();
+    }
+
+    fallback() external payable {
+        deposit();
+    }
+}
